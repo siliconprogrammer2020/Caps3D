0a9a65300d9d9edf72b4ac8a8b01a22385c80576
Sergio Fernandez <72816448+siliconprogrammer2020@users.noreply.github.com>
2020-10-24 17:29:46 -0400

    Add files via upload
    TPoint is a simple object representing a point on the screen.

---
+++ TPoint.php
@@ -0,0 +1,42 @@
+function TPoint(x,y) {
+   this.x=0;
+   this.y=0;
+
+   if (arguments.length === 1 ) { 
+     this.assign(x);
+   } else if (arguments.length === 2) {
+     this.assign(x,y);
+   };
+}
+
+TPoint.prototype.assign = function(x,y) {
+  if (arguments.length === 1) {
+    this.x=x.x;
+    this.y=x.y;
+  } else {
+     this.x=x;
+     this.y=y;
+  }
+};
+
+TPoint.prototype.sub = function(x,y) {
+   if (arguments.length === 1) {
+       return new TPoint(this.x-x.x,this.y-x.y);
+   } else {
+       return new TPoint(this.x-x,this.y-y);
+   }
+};
+
+TPoint.prototype.add = function(x,y) {
+   if (arguments.length === 1) {
+       return new TPoint(this.x+x.x,this.y+x.y);
+   } else {
+       return new TPoint(this.x+x,this.y+y);
+   }
+};
+
+TPoint.prototype.equals = function(p) {
+    return (this.x == p.x) && (this.y == p.y);
+};
+
+
