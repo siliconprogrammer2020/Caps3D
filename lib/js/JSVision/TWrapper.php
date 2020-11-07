3a009be65fae57a3d187002f3e7bedfd521730dd
Sergio Fernandez <72816448+siliconprogrammer2020@users.noreply.github.com>
2020-11-07 10:39:24 -0500

    Update TWrapper.php

--- lib/js/JSVision/TWrapper.php
+++ lib/js/JSVision/TWrapper.php
@@ -1,1 +1,11 @@
 
+TWrapper = function(content) {
+  TView.call(this);
+  this.content = content;
+};
+   extend(TWrapper,TView); //  inherit TApplet
+
+
+TWrapper.prototype.style = function() {
+  return "position:absolute; left:"+this.origin.x+"px; top:"+this.origin.y+"px; "+this._style;
+};
