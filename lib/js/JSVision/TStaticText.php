68d95fec375f0d066286c2d895bfd557e50dc559
Sergio Fernandez <72816448+siliconprogrammer2020@users.noreply.github.com>
2020-11-10 16:40:30 -0500

    Create TStaticText.php

---
+++ lib/js/JSVision/TStaticText.php
@@ -0,0 +1,11 @@
+function TStaticText(bounds,text) {  
+  TView.call(this,bounds); 
+  this.content = text;
+};
+
+extend(TStaticText,TView); //  inherit TApplet
+
+
+TStaticText.prototype.style = function() {
+  return TView.prototype.style.call(this)+' font-family:'+document.body.style.fontFamily+'; color:white; text-shadow: 2px 2px black;';
+};
